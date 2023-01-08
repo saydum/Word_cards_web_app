0388dc6776b66dc516e4c0ed2efa6fd5337f0a01
Saydum Khalibekov <saydum.it5@gmail.com>
2023-01-08 13:18:35 +0300

    feat(api): create (Card, Word, Example) API controllers

---
+++ app/Http/Controllers/Api/V1/WordController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Word;
+use Illuminate\Http\Request;
+
+class WordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Word $word)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Word $word)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Word $word)
+    {
+        //
+    }
+}
