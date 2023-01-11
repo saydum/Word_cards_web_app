03365afe265328ceec6dfb0f2fad2eee93427dac
Saydum Khalibekov <saydum.it5@gmail.com>
2023-01-11 23:25:09 +0300

    refactor(api): added card word example from api.php

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,17 @@ use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\CardController;
 use App\Http\Controllers\Api\V1\ExampleController;
 use App\Http\Controllers\Api\V1\WordController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
 });
 
-Route::resource('cards.api', CardController::class);
-Route::resource('words.api', WordController::class);
-Route::resource('examples.api', ExampleController::class);
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::resource('cards', CardController::class);
+    Route::resource('words', WordController::class);
+    Route::resource('examples', ExampleController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,1 @@
 <?php
-
-use App\Http\Controllers\CardController;
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\WordController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [HomeController::class, 'index'])->name('index');
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('cards', CardController::class);
-    Route::resource('words', WordController::class);
-    Route::resource('example', ExampleController::class);
-});
-
-Auth::routes();
